ce403dfa3d83e4f5cc7104d66fd1b5c6a763eae0
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-11-16 15:28:31 -0500

    created idea form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,99 +1,158 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Voting App</title>
+    <title>Voting App</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-
-
-        <!-- Fonts -->
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans"/>
 
-    <body class="font-sans bg-gray-background text-gray-900 text-sm">
-        <header class="flex items-center justify-between px-8 py-4">
-            <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
-            <div class="flex items-center">
-                    @if (Route::has('login'))
-                        <div class="px-6 py-4">
-                            @auth
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
 
-                                    <a href="{{ route('logout') }}"
-                                                     onclick="event.preventDefault();
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans bg-gray-background text-gray-900 text-sm">
+<header class="flex items-center justify-between px-8 py-4">
+    <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
+    <div class="flex items-center">
+        @if (Route::has('login'))
+            <div class="px-6 py-4">
+                @auth
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </a>
-                                </form>
-                            @else
-                                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                                @endif
-                            @endauth
-                        </div>
-                    @endif
-
-                        <a href="#">
-                            <img src="https://www.gravatar.com/avatar/000000000000000000000000000000000?d=mp"
-                                alt="avatar" class="rounded-full w-10 h-10">
+                            {{ __('Log Out') }}
                         </a>
+                    </form>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                           class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
             </div>
-        </header>
+        @endif
+
+        <a href="#">
+            <img src="https://www.gravatar.com/avatar/000000000000000000000000000000000?d=mp"
+                 alt="avatar" class="rounded-full w-10 h-10">
+        </a>
+    </div>
+</header>
 
 
-        <main class="container mx-auto max-w-custom flex">
-            <div class="w-70 mr-5">
+<main class="container mx-auto max-w-custom flex">
+    <div class="w-70 mr-5">
+        <div
+                class="bg-white border-2 border-blue rounded-xl mt-16"
+                style="
+            border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+            border-image-slice: 1;
+            background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+            background-origin: border-box;
+            background-clip: content-box, border-box;
+          "
+            >
 
-                Add IDEA FORM GOES HERE Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-
+            <div class="text-center px-6 py-2 pt-6">
+                <h3 class="font-semibold text-base">Add an idea</h3>
+                <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
             </div>
 
-            <div class="w-175">
-
-                <nav class="flex items-center justify-between text-xs">
-                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
-                        <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
-                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a></li>
-                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a></li>
-                    </ul>
-
-                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
-                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a></li>
-                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a></li>
-                    </ul>
-
-                    </ul>
-                </nav>
-
-                <div class="mt-8">
-                    {{ $slot }}
+            <form action="#" method="POST" class="space-y-4 px-4 py-6">
+                <div>
+                    <input type="text" class="w-full bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2"
+                           placeholder="Your Idea">
                 </div>
+                <div>
+                    <select name="category_add" id="category_add"
+                            class="w-full bg-gray-100 rounded-xl border-none px-4 py-2 text-sm">
+                        <option value="Category One">Category One</option>
+                        <option value="Category Two">Category Two</option>
+                        <option value="Category Three">Category Three</option>
+                        <option value="Category Four">Category Four</option>
+                    </select>
+                </div>
+                <div>
+                    <textarea name="idea" id="idea" cols="30" rows="4"
+                              class="w-full bg-gray-100 rounded-xl border-placeholder-gray-900 text-sm px-4 py-2"
+                              placeholder="Describe your idea"></textarea>
+                </div>
+                <div class="flex items-center justify-between space-x-3">
+                    <button
+                        type="button"
+                        class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px6 py-3">
 
-            </div>
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                             class="text-gray-600 w-4 transform -rotate-45">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"/>
+                        </svg>
+
+
+                        <span class="ml-1">Attach</span>
+
+                    </button>
+
+                    <button
+                        type="button"
+                        class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border bg-blue text-white transition duration-150 ease-in px6 py-3">
+
+
+                        <span class="ml-1">Submit</span>
+
+                    </button>
+
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="w-175">
+
+        <nav class="flex items-center justify-between text-xs">
+            <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering
+                        (6)</a></li>
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In
+                        Progress (1)</a></li>
+            </ul>
+
+            <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented
+                        (10)</a></li>
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed
+                        (55)</a></li>
+            </ul>
+
+            </ul>
+        </nav>
+
+        <div class="mt-8">
+            {{ $slot }}
+        </div>
+    </div>
 
 
 
 
-        </main>
+</main>
 
-    </body>
+</body>
 </html>
 
 
