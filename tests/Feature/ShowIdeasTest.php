c10d026ae025d182a56dc6d33770b1d1fa686534
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-11-23 09:32:26 -0500

    write unit tests showing ideas

---
+++ tests/Feature/ShowIdeasTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Idea;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ShowIdeasTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_list_of_ideas_shows_on_main_page()
+    {
+
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My Second Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee($ideaOne->title);
+        $response->assertSee($ideaOne->description);
+        $response->assertSee($ideaTwo->title);
+        $response->assertSee($ideaTwo->description);
+
+    }
+
+
+    public function test_single_idea_shows_correctly_on_the_show_page()
+    {
+
+        $idea = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+
+        $idea = Idea::factory()->create([
+            'title' => 'My Second Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+//dd(route('idea.show', $idea->id  ));
+        $response = $this->get(route('idea.show', $idea->slug  ));
+
+        $response->assertSuccessful();
+        $response->assertSee($idea->title);
+        $response->assertSee($idea->description);
+
+
+    }
+
+    public function test_ideas_pagination_works()
+    {
+
+        Idea::factory(Idea::PAGINATION_COUNT + 1)->create();
+
+        $ideaOne = Idea::find(1);
+        $ideaOne->title = "My first Idea";
+        $ideaOne->save();
+
+        $ideaEleven = Idea::find(11);
+        $ideaEleven->title = 'My Eleventh Idea';
+        $ideaEleven->save();
+
+        $response = $this->get('/');
+
+        $response->assertSee($ideaOne->title);
+        $response->assertDontSee($ideaEleven->title);
+
+        $response = $this->get('/?page=2');
+
+        $response->assertSee($ideaEleven->title);
+        $response->assertDontSee($ideaOne->title);
+
+    }
+}
