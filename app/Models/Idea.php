7d5fa9b298e1c03dbf0358fb55a1c9f012b2894d
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-11-30 09:50:18 -0500

    fixed status and description database issues and submit idea button issue

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -37,4 +37,16 @@ class Idea extends Model
         return $this->belongsTo(User::class);
     }
 
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
 }
