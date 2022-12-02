141a7555455f9bc6097446369127b510faa8c677
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-12-02 16:51:01 -0500

    added category filtering

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -2,32 +2,25 @@
 
 namespace App\Http\Livewire;
 
-
+use App\Models\Idea;
 use App\Models\Status;
 use Illuminate\Support\Facades\Route;
 use Livewire\Component;
 
 class StatusFilters extends Component
 {
-
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-      'status',
-    ];
-
-
 
 
     public function mount()
     {
 
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
@@ -35,14 +28,14 @@ class StatusFilters extends Component
     public function setStatus($newStatus)
     {
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
-//        if ($this->getPreviousRouteName() === 'idea.show') {
+        if ($this->getPreviousRouteName() === 'idea.show') {
             return redirect()->route('idea.index', [
-               'status' => $this->status,
-
+                'status' => $this->status,
             ]);
-//        }
 
+        }
     }
 
 
