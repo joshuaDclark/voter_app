86e41a9bed704426c4460898eb653330b892cab3
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-12-05 12:09:10 -0500

    set other filters

--- app/Http/Livewire/Ideasindex.php
+++ app/Http/Livewire/Ideasindex.php
@@ -15,12 +15,13 @@ class Ideasindex extends Component
 
     public $status;
     public $category;
-//    public $page;
+    public $filter;
 
 
     protected $queryString = [
         'status',
         'category',
+        'filter',
     ];
 
     protected  $listeners = ['queryStringUpdatedStatus'];
@@ -38,6 +39,16 @@ class Ideasindex extends Component
     {
         $this->resetPage();
 
+    }
+
+
+    public function updatedFilter()
+    {
+        if ($this->filter === 'My Ideas') {
+            if (! auth()->check()) {
+                return redirect()->route('login');
+            }
+        }
     }
 
 
@@ -63,6 +74,12 @@ class Ideasindex extends Component
                 })
                 ->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
+                })
+                ->when($this->filter && $this->filter === 'Top Voted', function ($query) {
+                    return $query->orderByDesc('votes_count');
+                })
+                ->when($this->filter && $this->filter === 'My Ideas', function ($query) {
+                    return $query->where('user_id', auth()->id());
                 })
                 ->addSelect(['voted_by_user' => Vote::select('id')
                     ->where('user_id', auth()->id())
